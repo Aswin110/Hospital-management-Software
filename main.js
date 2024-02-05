6fd0246baac198b06bf71b5d936c282af259d877
Aswin110 <aswinashok110@gmail.com>
2024-02-06 00:43:09 +0530

    finish navbar compatible for desktop and mobile screens

---
+++ main.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const toggleButton = document.querySelector(
+    '[data-collapse-toggle="navbar-default"]'
+  );
+  const navbarElement = document.getElementById("navbar-default");
+
+  if (toggleButton && navbarElement) {
+    toggleButton.addEventListener("click", function () {
+      navbarElement.classList.toggle("hidden");
+    });
+  }
+});
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{html,js}",
+    "./pages/**/*.{html,js}",
+    "./components/**/*.{html,js}",
+    "./**/*.{html,js}",
+    "./index.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
